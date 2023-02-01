710516fb80ca3f4a96afa347f8ec37760ebf3ebe
xXLeciXx <124141088+xXLeciXx@users.noreply.github.com>
2023-02-01 06:32:49 -0300

    Add files via upload

---
+++ function.js
@@ -0,0 +1,23 @@
+function evento() {
+    alert("Mensaje enviado con exito!")
+}
+
+document.getElementById("boton_ocultar").addEventListener("click", function() {
+    document.getElementById("Seccion1").style.display = "none";
+
+});
+
+document.getElementById("boton_ocultar2").addEventListener("click", function() {
+    document.getElementById("Seccion2").style.display = "none";
+
+});
+
+document.getElementById("boton_ocultar3").addEventListener("click", function() {
+    document.getElementById("Seccion3").style.display = "none";
+
+});
+
+document.getElementById("boton_ocultar4").addEventListener("click", function() {
+    document.getElementById("Seccion4").style.display = "none";
+
+});
